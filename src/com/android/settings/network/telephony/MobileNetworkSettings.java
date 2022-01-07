83b914cc731465a8204e81f655d30412627db192
jhonboy121 <alfredmathew05@gmail.com>
2022-01-07 21:13:32 +0530

    Settings: fix edit menu icon tints
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- src/com/android/settings/network/telephony/MobileNetworkSettings.java
+++ src/com/android/settings/network/telephony/MobileNetworkSettings.java
@@ -22,6 +22,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -380,6 +381,9 @@ public class MobileNetworkSettings extends AbstractMobileNetworkSettings {
             final MenuItem item = menu.add(Menu.NONE, R.id.edit_sim_name, Menu.NONE,
                     R.string.mobile_network_sim_name);
             item.setIcon(com.android.internal.R.drawable.ic_mode_edit);
+            item.setIconTintList(ColorStateList.valueOf(
+                com.android.settingslib.Utils.getColorAttrDefaultColor(getContext(),
+                    android.R.attr.colorControlNormal)));
             item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         }
         super.onCreateOptionsMenu(menu, inflater);
