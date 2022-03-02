011f6f815b8583219ab9a9efcc8c8ebb35298069
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-03-02 17:52:19 +0800

    SoftAp: add Hidden SSID preference
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: I8a222a9a9ae8f005953b738368168d551a502491

--- tests/robotests/src/com/android/settings/wifi/tether/WifiTetherSettingsTest.java
+++ tests/robotests/src/com/android/settings/wifi/tether/WifiTetherSettingsTest.java
@@ -102,6 +102,7 @@ public class WifiTetherSettingsTest {
         assertThat(niks).doesNotContain(WifiTetherSettings.KEY_WIFI_TETHER_NETWORK_PASSWORD);
         assertThat(niks).doesNotContain(WifiTetherSettings.KEY_WIFI_TETHER_AUTO_OFF);
         assertThat(niks).doesNotContain(WifiTetherSettings.KEY_WIFI_TETHER_NETWORK_AP_BAND);
+        assertThat(niks).doesNotContain(WifiTetherSettings.KEY_WIFI_TETHER_HIDDEN_SSID);
     }
 
     @Test
@@ -116,6 +117,7 @@ public class WifiTetherSettingsTest {
         assertThat(niks).contains(WifiTetherSettings.KEY_WIFI_TETHER_NETWORK_PASSWORD);
         assertThat(niks).contains(WifiTetherSettings.KEY_WIFI_TETHER_AUTO_OFF);
         assertThat(niks).contains(WifiTetherSettings.KEY_WIFI_TETHER_NETWORK_AP_BAND);
+        assertThat(niks).contains(WifiTetherSettings.KEY_WIFI_TETHER_HIDDEN_SSID);
     }
 
     @Test
